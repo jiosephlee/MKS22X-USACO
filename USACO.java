665a93d6ff1d86aba237930dd6387585e955f639
John Dogit config --global user.name John <jlee01@stuy.edu>
2019-03-07 10:14:14 -0500

    okay im dumb. i really spent two days writing a readfile function that scanner already had.. FINISHED BRONZED FINALLY

--- USACO.java
+++ USACO.java
@@ -2,47 +2,66 @@ import java.io.*;
 import java.util.*;
 public class USACO{
     public static void main(String[] args) throws FileNotFoundException{
-        bronze("makelake.1.in");
+        System.out.println(bronze("makelake.1.in"));
     }
     public static int bronze(String filename) throws FileNotFoundException{
         File text = new File(filename);
         // can be a path like: "/full/path/to/file.txt" or "../data/file.txt"
 
         //inf stands for the input file
-        Scanner inf = new Scanner(text);
-        String lineone = inf.nextLine();
-        String[] elements = new String[]{"","","",""}; //R, C, E, N
-        for(int i = 0, index = 0; i < lineone.length(); i++){
-            if(lineone.charAt(i) == ' '){
-                index++;
-                //System.out.println(index);
-            } else{
-                elements[index]+=lineone.substring(i,i+1);
-                //System.out.println(elements[index]);
-            }
-        }
-        int R = Integer.parseInt(elements[0]);
-        int C = Integer.parseInt(elements[1]);
-        int E = Integer.parseInt(elements[2]);
-        int N =Integer.parseInt(elements[3]);
+        Scanner in = new Scanner(text);
+
+        int R = Integer.parseInt(in.next());
+        int C = Integer.parseInt(in.next());
+        int E = Integer.parseInt(in.next());
+        int N = Integer.parseInt(in.next());
         int[][] board = new int[R][C];
         int[][] instructions = new int[N][3];
         for (int xcor = 0; xcor < R; xcor++){
-            String line = inf.nextLine();
-            for (int ycor = 0; ycor < line.length(); ycor++){
-                board[xcor][ycor] = line.charAt(ycor);
-                System.out.print(board[xcor][ycor]);
+            for (int ycor = 0; ycor < C; ycor++){
+                board[xcor][ycor] = Integer.parseInt(in.next());
+                //System.out.print(board[xcor][ycor]);
             }
-            System.out.println();
+            //System.out.println();
         }
         for (int xcor = 0; xcor < N; xcor++){
-            String line = inf.nextLine();
             for (int ycor = 0; ycor < 3; ycor++){
-                instructions[xcor][ycor] = line.charAt(ycor);
-                System.out.print(instructions[xcor][ycor]);
+                instructions[xcor][ycor] = Integer.parseInt(in.next());
+                //System.out.print(instructions[xcor][ycor]);
             }
-            System.out.println();
+            //System.out.println();
         }
-        return -1;
+        return solveBronze(board, E, instructions);
+    }
+    public static int solveBronze(int[][] board, int E, int[][] instructions){
+        for (int[] i : instructions){
+            int max = 0;
+            for(int xcor = i[0] - 1; xcor < i[0] + 2; xcor++){
+                for(int ycor = i[1] - 1; ycor < i[1] + 2; ycor++){
+                    if (board[xcor][ycor] > max){
+                        max = board[xcor][ycor];
+                    }
+                }
+            }
+            max = max - i[2];
+            for(int xcor = i[0] - 1; xcor < i[0] + 2; xcor++){
+                for(int ycor = i[1] - 1; ycor < i[1] + 2; ycor++){
+                    if (board[xcor][ycor] > max){
+                        board[xcor][ycor] = max;
+                    }
+                }
+            }
+        }
+        int sum = 0;
+        for (int x = 0; x < board.length; x++){
+            for (int y = 0; y < board[0].length; y++){
+                //System.out.print(E - board[x][y]);
+                if (E - board[x][y] > 0){
+                    sum+= E - board[x][y];
+                }
+            }
+            //System.out.println();
+        }
+        return sum * 72 * 72;
     }
 }
